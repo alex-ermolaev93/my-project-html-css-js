0287b4c218eadbdfd238fe215adbf963d6a4e30a
Aleksandr Yermolaev <ermolaev.jp@gmail.com>
2019-03-25 02:52:07 +0200

    fixed and next try 4

---
+++ prompt.js
@@ -0,0 +1,14 @@
+var i = +prompt('number?','');
+switch (i) {
+    case 5:
+        console.log('ok');
+        break;
+    case 9:
+        console.log('okey');
+        break;
+    case 15:
+        console.log('not ok');
+        break;
+    default:
+        console.log('no no no!!!')
+}
